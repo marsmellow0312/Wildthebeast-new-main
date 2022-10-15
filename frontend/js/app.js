0ffb92bc120830aaaf3736a9ca093ca3313b82bf
Alvindra Aditya <alvindra14@gmail.com>
2022-10-16 03:44:22 +0700

    ASD

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -140,7 +140,7 @@ async function checkChain() {
                 {
                   chainName: 'Polygon Mainnet',
                   chainId: web3.utils.toHex(chainId),
-                  nativeCurrency: { name: 'WETH', decimals: 18, symbol: 'WETH' },
+                  nativeCurrency: { name: 'MATIC', decimals: 18, symbol: 'MATIC' },
                   rpcUrls: ['https://polygon-rpc.com/'],
                   // rpcUrls: ['https://polygon-mainnet.infura.io/v3/295cce92179b4be498665b1b16dfee34'],
                   
