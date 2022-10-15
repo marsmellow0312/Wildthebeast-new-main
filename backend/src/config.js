7f08caeb185c1405b181f052664aac06a3e5d282
Alvindra Aditya <alvindra14@gmail.com>
2022-10-15 21:35:39 +0700

    asd

--- backend/src/config.js
+++ backend/src/config.js
@@ -111,7 +111,7 @@ const TOKENS_PER_MINT = 5000; // maximum number of NFTs a user can mint in a sin
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER. 
 const PUBLIC_MINT_START_DATE = "2022-10-25T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2022-10-20T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-10-14T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 250; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x627347A67D1538B02B4374a227e6eB18c820D68A"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
